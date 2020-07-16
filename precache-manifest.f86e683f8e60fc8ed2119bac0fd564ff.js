b740eeb3bbbe91eab8e1fcb0c58a057d63d571e8
Abu <63474683+Abumuazu@users.noreply.github.com>
2020-07-15 22:27:00 -0700

    Updates

---
+++ precache-manifest.f86e683f8e60fc8ed2119bac0fd564ff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e934cddddb0b85bd02d93248cd6e3381",
+    "url": "/Muaz-portfolio/index.html"
+  },
+  {
+    "revision": "0552c4154ce478dcb4a4",
+    "url": "/Muaz-portfolio/static/css/main.b1f91ab1.chunk.css"
+  },
+  {
+    "revision": "6d5a463f67e7b8bdbcca",
+    "url": "/Muaz-portfolio/static/js/2.f766b94d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Muaz-portfolio/static/js/2.f766b94d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0552c4154ce478dcb4a4",
+    "url": "/Muaz-portfolio/static/js/main.ad93713c.chunk.js"
+  },
+  {
+    "revision": "56445a12b18d7fe2fe5f",
+    "url": "/Muaz-portfolio/static/js/runtime-main.78a200b1.js"
+  },
+  {
+    "revision": "ad05284770ef91d65c70b4f7040239e9",
+    "url": "/Muaz-portfolio/static/media/github.ad052847.svg"
+  },
+  {
+    "revision": "bd965c8121ea35d35cdc1606a9835e2e",
+    "url": "/Muaz-portfolio/static/media/instagram.bd965c81.svg"
+  },
+  {
+    "revision": "12e9c34ea2d6d91c1ee62b67c8d0096d",
+    "url": "/Muaz-portfolio/static/media/linkedin.12e9c34e.svg"
+  },
+  {
+    "revision": "c4c914f8caac9efceb19e9c4a610a043",
+    "url": "/Muaz-portfolio/static/media/twitter.c4c914f8.svg"
+  }
+]);
